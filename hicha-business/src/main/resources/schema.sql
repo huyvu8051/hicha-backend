5d73283a74d755e6a4ac533880e234af82a4ae7b
huyvu8051 <huyvu8051@gmail.com>
2024-04-19 23:46:51 +0700

    interate partial test and full setup test

--- hicha-business/src/main/resources/schema.sql
+++ hicha-business/src/main/resources/schema.sql
@@ -2,4 +2,6 @@
 create table if not exists user(
     id int primary key,
     name varchar(255) not null
-)
+);
+
+insert into user values (21, 'Tùng Dương')
--- hicha-business/src/test/java/io/huyvu/hicha/hichabusiness/HichaBusinessApplicationTests.java
+++ hicha-business/src/test/java/io/huyvu/hicha/hichabusiness/HichaBusinessApplicationTests.java
@@ -1,6 +1,9 @@
 package io.huyvu.hicha.hichabusiness;
 
+import io.huyvu.hicha.hichabusiness.model.UserDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,28 +13,33 @@ import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.List;
+
 /*
  * Integration Test
  */
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
 class HichaBusinessApplicationTests {
-
-	/*@Test
-	void contextLoads() {
-	}*/
 
 	@Container
 	@ServiceConnection
-	static MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>("mariadb:latest");
+	static MariaDBContainer<?> mariaDBContainer =  new MariaDBContainer<>("mariadb:latest")
+			.withDatabaseName("hicha")
+			.withUsername("myuser")
+			.withPassword("secret");
 
 	@Autowired
 	TestRestTemplate restTemplate;
 
+
+
 	@Test
 	void returnHelloWorld(){
 		String response = restTemplate.getForObject("/api/v1", String.class);
-		Assertions.assertThat(response).isEqualTo("Hello World!");
+		log.info(response);
+		Assertions.assertThat(response).isNotEmpty();
 	}
 
 }
