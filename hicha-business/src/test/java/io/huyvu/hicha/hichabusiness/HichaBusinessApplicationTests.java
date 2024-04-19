43650a5a3377bf5c1755294c8400fa3d5747fa02
huyvu8051 <huyvu8051@gmail.com>
2024-04-19 22:09:15 +0700

    setup integration test

--- hicha-business/src/test/java/io/huyvu/hicha/hichabusiness/HichaBusinessApplicationTests.java
+++ hicha-business/src/test/java/io/huyvu/hicha/hichabusiness/HichaBusinessApplicationTests.java
@@ -1,13 +1,37 @@
 package io.huyvu.hicha.hichabusiness;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+/*
+ * Integration Test
+ */
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HichaBusinessApplicationTests {
 
 	/*@Test
 	void contextLoads() {
 	}*/
+
+	@Container
+	@ServiceConnection
+	static MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>("mariadb:latest");
+
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	@Test
+	void returnHelloWorld(){
+		String response = restTemplate.getForObject("/api/v1", String.class);
+		Assertions.assertThat(response).isEqualTo("Hello World!");
+	}
 
 }
