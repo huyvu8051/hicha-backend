6c8f91af74d7620f8bfe8f534c5832984b0885a3
huyvu8051 <huyvu8051@gmail.com>
2024-04-20 15:42:29 +0700

    setup zipkin observe

--- hicha-business/src/main/java/io/huyvu/hicha/hichabusiness/HichaBusinessApplication.java
+++ hicha-business/src/main/java/io/huyvu/hicha/hichabusiness/HichaBusinessApplication.java
@@ -1,7 +1,16 @@
 package io.huyvu.hicha.hichabusiness;
 
+import io.huyvu.hicha.hichabusiness.service.JsonPlaceholderService;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.annotation.Observed;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
 public class HichaBusinessApplication {
@@ -10,4 +19,19 @@ public class HichaBusinessApplication {
 		SpringApplication.run(HichaBusinessApplication.class, args);
 	}
 
+	@Bean
+	JsonPlaceholderService jsonPlaceholderService() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com");
+		HttpServiceProxyFactory fact = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return fact.createClient(JsonPlaceholderService.class);
+	}
+
+	@Bean
+
+	@Observed(name = "posts.load-all-posts", contextualName = "post.find-all")
+	CommandLineRunner commandLineRunner(JsonPlaceholderService jsonPlaceholderService, ObservationRegistry observationRegistry) {
+		return args -> {
+			jsonPlaceholderService.findAll();
+		};
+	}
 }
