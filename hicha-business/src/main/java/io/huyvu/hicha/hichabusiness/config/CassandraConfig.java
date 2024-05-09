699c27406dea2dee17a5490ffb74ff8844cb2130
Vũ Văn Huy <huyvu8051@gmail.com>
2024-05-09 09:13:17 +0700

    split deploy

--- hicha-business/src/main/java/io/huyvu/hicha/hichabusiness/config/CassandraConfig.java
+++ hicha-business/src/main/java/io/huyvu/hicha/hichabusiness/config/CassandraConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.util.List;
+
 @Configuration
 public class CassandraConfig {
-//    @Bean
-//    @Primary
-    CassandraConnectionDetails mainConnection(CassandraConnectionDetails cassandraConnectionDetailsForD1r1n1){
-        return cassandraConnectionDetailsForD1r1n1;
+    @Bean
+    @Primary
+    CassandraConnectionDetails mainConnection(List<CassandraConnectionDetails> conns){
+        return conns.getFirst();
     }
 }
