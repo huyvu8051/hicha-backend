8f9411c171b10e49eb13e87be71f085a4f4e43ea
Vũ Văn Huy <huyvu8051@gmail.com>
2024-05-17 14:56:14 +0700

    finish sub api

--- hicha-business/src/main/java/io/huyvu/hicha/UnknownSourceTargetType.java
+++ hicha-business/src/main/java/io/huyvu/hicha/UnknownSourceTargetType.java
@@ -1,5 +1,8 @@
 package io.huyvu.hicha;
 
+import io.huyvu.hicha.model.MessageDTO;
+import io.huyvu.hicha.repository.model.Message;
+
 public class UnknownSourceTargetType {
 
     @SuppressWarnings("unchecked")
@@ -7,34 +10,35 @@ public class UnknownSourceTargetType {
         return (T) source;
     }
 
-    public <T> KnownTargetType<T> to(Class<T> targetType) {
-        return new KnownTargetType<>(targetType);
-    }
-
-    public <S> KnownSourceType<S> map(Class<S> sourceType) {
-        return new KnownSourceType<>(sourceType);
-    }
-
-
     public static class KnownTargetType<T> {
-
+        private final Class<T> targetType;
         public KnownTargetType(Class<T> targetType) {
-
+            this.targetType = targetType;
         }
 
         @SuppressWarnings("unchecked")
         public T from(Object source) {
             return (T) source;
         }
+
+        public <S> KnownSourceTargetType<S, T> map(Class<S> sourceType) {
+            return new KnownSourceTargetType<>(sourceType, targetType);
+        }
     }
 
     public static class KnownSourceType<S> {
+        private final Class<S> sourceType;
         public KnownSourceType(Class<S> sourceType){
+            this.sourceType = sourceType;
         }
 
         @SuppressWarnings("unchecked")
         public <T> T from(S source){
             return (T) source;
+        }
+
+        public <T> KnownSourceTargetType<S, T> to(Class<T> targetType) {
+            return new KnownSourceTargetType<>(sourceType, targetType);
         }
     }
 
--- hicha-business/src/main/java/io/huyvu/hicha/controller/MessageController.java
+++ hicha-business/src/main/java/io/huyvu/hicha/controller/MessageController.java
@@ -30,12 +30,16 @@ public class MessageController {
         Message msg1 = new UnknownSourceTargetType().from(dto);
 
         Message msg2 = new KnownTargetType<>(Message.class).from(dto);
+        Message msg3 = new KnownTargetType<>(Message.class).map(MessageDTO.class).from(dto);
 
-        Message from = new KnownSourceType<>(MessageDTO.class).from(dto);
-        MessageRepository from3 = new KnownSourceType<>(MessageDTO.class).from(dto);
+
 
         Message from2 = new KnownSourceTargetType<>(MessageDTO.class, Message.class).from(dto);
 
+
+        Message from = new KnownSourceType<>(MessageDTO.class).from(dto);
+        MessageRepository from3 = new KnownSourceType<>(MessageDTO.class).from(dto);
+        Message from1 = new KnownSourceType<>(MessageDTO.class).to(Message.class).from(dto);
 
 
         entity.setSentAt(Instant.now());
